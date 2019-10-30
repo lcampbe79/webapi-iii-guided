bc8452d62ec981797c65ec7c167405869a755b1d
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-10-30 10:05:59 -0700

    Logger.

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+function dateLog(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.route(
+      'Origin'
+    )}`
+  );
+
+  next();
+}
+
+//global middleware
+server.use(helmet());
 server.use(express.json());
+server.use(dateLog)
 
 server.use('/api/hubs', hubsRouter);
 
