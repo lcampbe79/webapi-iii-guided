300772a825c4b6f49222dae6d148776e88f4a475
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-10-30 10:32:13 -0700

    Completed updating gateKeeper.

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
+const morgan = require('morgan');
+
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-function dateLog(req, res, next) {
+//three amigos
+function dateLogger(req, res, next) {
+  console.log(new Date().toISOString());
+  next();
+}
+
+function logger(req, res, next) {
   console.log(
-    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.route(
-      'Origin'
-    )}`
+    `The Logger: [${new Date().toISOString()}] ${req.method} to ${req.url})}`
   );
 
   next();
 }
 
+function gateKeeper(req, res, next) {
+  const password = req.headers.password || '';
+
+  if (!req.headers.password) {
+    res.status(400).json({message: "Please provide a password"})
+  } else {
+    if (password.toLowerCase() === 'mellon') {
+      next();
+    } else {
+      res.status(400).json({you: "You shall not pass!"})
+    }
+  }
+
+}
+
 //global middleware
+
 server.use(helmet());
 server.use(express.json());
-server.use(dateLog)
+//server.use(dateLogger)
+server.use(gateKeeper);
+server.use(logger)
+server.use(morgan('dev'))
 
 server.use('/api/hubs', hubsRouter);
 
